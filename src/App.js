3c5c9f34a81a33f921cef460ca89c8b3dbb45958
subham-202 <shubham.deep.202@gmail.com>
2023-02-03 14:21:35 +0530

    basic ui-done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { BrowserRouter as Router,Route, Routes } from "react-router-dom"
+import Footer from "./Components/Footer"
+import Navbar from "./Components/Navbar"
+import About from "./Pages/About"
+import Home from "./Pages/Home"
+import NotFound from "./Pages/NotFound"
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/about" element={<About/>}/>
+              <Route path="/*" element={<NotFound/>}/>
+              <Route path="/n otFound" element={<NotFound/>}/>
+            </Routes>
+            </main>
+          <Footer />
+        </div>
+      </Router>
     </div>
-  );
+  )
 }
-
-export default App;
